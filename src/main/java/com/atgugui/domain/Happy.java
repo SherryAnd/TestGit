984f9d106784b89b246747b8ccf972355fe9c002
tom_glb <goodMorning_pro@atguigu.com>
2019-07-11 10:45:55 +0800

    edit on branch hot_fix

--- src/main/java/com/atgugui/domain/Happy.java
+++ src/main/java/com/atgugui/domain/Happy.java
@@ -2,8 +2,8 @@ package com.atgugui.domain;
 
 public class Happy {
 	public static void main(String[] args) {
-		System.out.println("right ...");
-		
+		System.out.println("left....");
+		System.out.println("hot fix ...");
 	}
 
 }
